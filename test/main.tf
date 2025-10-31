894f03e6f37c3fdb044da86dbe93e3311f816124
Balakrishnacn <balakrishna-reddy.c-n@capgemini.com>
2025-10-31 20:41:48 +0530

    check_04

--- test/main.tf
+++ test/main.tf
@@ -1,13 +1,23 @@
+
 provider "aws" {
-  region = "us-east-1"  # Change to your preferred region
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"  # Bucket name must be globally unique
-  acl    = "private"
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-unique-bucket-9781a717-f497-4272-9961-bebf18374868"
+  acl    = "public-read"
 
   tags = {
-    Name        = "MyBucket"
+    Name        = "PublicBucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
